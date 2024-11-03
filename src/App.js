5106891291800ced13a312f61367355993354c94
westt <westtonyemm@gmail.com>
2024-11-03 14:27:44 -0500

    commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <img src={logo} alt="React Logo" className="logo" />
+        <h1>Welcome to Fullstack Development - I</h1>
+        <h2>React JS Programming Week09 Lab exercise</h2>
+        <p>101395382</p>
+        <p>Michael West</p>
+        <p>George Brown College, Toronto</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
